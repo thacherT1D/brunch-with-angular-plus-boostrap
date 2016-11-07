85fdfb6f6ff62223f8bef6d90547fc9e8f190969
THACHER <thachert1d@gmail.com>
2016-11-06 22:20:38 -0800

    testing ngRoute

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 import angular from 'angular'
 import bootstrap from 'bootstrap'
-import uiRouter from 'angular-ui-router'
+// import uiRouter from 'angular-ui-router'
+import ngRoute from 'angular-route'
 
-angular.module('my-app', [bootstrap, uiRouter])
+angular.module('my-app', [bootstrap, ngRoute])
   .config(['$stateProvider', ($stateProvider) => {
     $stateProvider
       .state('home', {
