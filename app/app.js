560477e86fd5bc8cb4fb5cc822ee16093b49b91a
THACHER <thachert1d@gmail.com>
2016-11-06 21:23:18 -0800

    uirouter config

--- app/app.js
+++ app/app.js
@@ -1,4 +1,17 @@
 import angular from 'angular'
 import bootstrap from 'bootstrap'
+import uiRouter from 'angular-ui-router'
 
-angular.module('my-app', [bootstrap]);
+angular.module('my-app', [bootstrap, uiRouter])
+  .config(['$stateProvider', ($stateProvider) => {
+    $stateProvider
+      .state('home', {
+        url: '',
+        templateUrl: 'views/main.html',
+      })
+      .state('main', {
+        url: '/',
+        templateUrl: 'views/main.html',
+      });
+
+    }]);
